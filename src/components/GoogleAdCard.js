cf382225624e6aedb0aee6e17ded701080305481
Bran0612 <branchembahbolo@gmail.com>
2023-06-22 13:55:30 -0400

    initial commit

---
+++ src/components/GoogleAdCard.js
@@ -0,0 +1,57 @@
+const ad = {
+    "ad_id": "CR05673361664385220609",
+    "ad_url": "https://adstransparency.google.com/advertiser/AR01861182914755035137/creative/CR05673361664385220609?political\u003d\u0026region\u003dEU",
+    "ad_type": "IMAGE",
+    "regions": "AT, BE, BG, CY, CZ, DE, DK, EE, ES, FI, FR, GB, GR, HR, HU, IE, IT, LT, LU, LV, MT, NL, PL, PT, RO, SE, SI, SK",
+    "advertiser_id": "AR01861182914755035137",
+    "advertiser_name": "PARTIDUL SOCIAL DEMOCRAT - P.S.D.",
+    "ad_campaigns_list": null,
+    "date_range_start": "2019-09-25",
+    "date_range_end": "2019-10-07",
+    "num_of_days": "13",
+    "impressions": "1750000-2000000",
+    "spend_usd": null,
+    "first_served_timestamp": "2019-09-25 07:00:00.000000 UTC",
+    "last_served_timestamp": "2019-10-07 07:00:00.000000 UTC",
+    "age_targeting": null,
+    "gender_targeting": null,
+    "geo_targeting_included": "Alba Iulia,Alba County,Romania, Alexandria,Teleorman County,Romania, Arad,Arad County,Romania, Bacau,Bacau County,Romania, Baia Mare,Maramures County,Romania, Botosani,Botosani County,Romania, Braila,Braila County,Romania, Brasov,Brasov County,Romania, Bucharest,Bucharest,Romania, Bucharest,Romania, Buzau,Buzau County,Romania, Calarasi,Calarasi County,Romania, Cluj-Napoca,Cluj County,Romania, Constanta,Constanta County,Romania, Craiova,Dolj County,Romania, Deva,Hunedoara County,Romania, Drobeta-Turnu Severin,Mehedinti County,Romania, Focsani,Vrancea County,Romania, Galati,Galati,Romania, Giurgiu,Giurgiu County,Romania, Henri Coanda International Airport,Ilfov County,Romania, Iasi,Iasi County,Romania, Ilfov County,Romania, Mangalia,Constanta County,Romania, Miercurea Ciuc,Harghita County,Romania, Oradea,Bihor County,Romania, Otopeni,Ilfov County,Romania, Piatra Neamt,Neamt County,Romania, Pitesti,Arges County,Romania, Ploiesti,Prahova,Romania, Ramnicu Valcea,Valcea County,Romania, Satu Mare,Satu Mare County,Romania, Sfantu Gheorghe,Covasna County,Romania, Sibiu,Sibiu,Romania, Sighisoara,Mures County,Romania, Slobozia,Ialomita County,Romania, Suceava,Suceava County,Romania, Targoviste,Dambovita County,Romania, Targu Mures,Mures County,Romania, Timisoara,Timis County,Romania, Tulcea,Tulcea County,Romania, Turda,Cluj County,Romania, Vaslui,Vaslui County,Romania, Zalau,Salaj,Romania",
+    "geo_targeting_excluded": null,
+    "spend_range_min_usd": "0",
+    "spend_range_max_usd": "100",
+    "spend_range_min_eur": "450",
+    "spend_range_max_eur": "500",
+    "spend_range_min_inr": "0",
+    "spend_range_max_inr": "250",
+    "spend_range_min_bgn": "0",
+    "spend_range_max_bgn": "100",
+    "spend_range_min_hrk": "0",
+    "spend_range_max_hrk": "0",
+    "spend_range_min_czk": "0",
+    "spend_range_max_czk": "1500",
+    "spend_range_min_dkk": "0",
+    "spend_range_max_dkk": "500",
+    "spend_range_min_huf": "0",
+    "spend_range_max_huf": "20000",
+    "spend_range_min_pln": "0",
+    "spend_range_max_pln": "250",
+    "spend_range_min_ron": "2000",
+    "spend_range_max_ron": "2250",
+    "spend_range_min_sek": "0",
+    "spend_range_max_sek": "1000",
+    "spend_range_min_gbp": "0",
+    "spend_range_max_gbp": "50",
+    "spend_range_min_nzd": "0",
+    "spend_range_max_nzd": "200",
+    "spend_range_min_brl": "0",
+    "spend_range_max_brl": "500"
+  }
+const GoogleAdCard = () => {
+    return (
+        <div>
+            <h1>add card here</h1>
+        </div>
+    )
+}
+
+export default GoogleAdCard;
